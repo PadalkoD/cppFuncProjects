9e690f651d6f36c63b62d88270395d32b8ed1b1e
PadalkoD <145016443+PadalkoD@users.noreply.github.com>
2023-09-28 14:34:55 +0300

    calculator switch case

---
+++ calculator switch case.cpp
@@ -0,0 +1,93 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+
+	float num1, num2;
+	char op;
+	cout << "input two number:  ";
+	cin >> num1 >> num2;
+	cout << "input operation:   ";
+	switch (op) {
+	case '+':
+		cout << num1 << "+" << num2 << "=" << (num1 + num2);
+		break;
+	case  '-':
+		cout << num1 << "-" << num2 << "=" << (num1 - num2);
+		break;
+	case '*':
+		cout << num1 << "*" << num2 << "=" << (num1 * num2);
+		break;
+	case '/':
+		(num2) ?
+			cout << num1 << "/" << num2 << "=" << (num1 / num2) :
+			cout << "division by zero";
+		break;
+	default:
+		cout << "incorrect operation";
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
