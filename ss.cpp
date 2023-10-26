6201269e1c5c47fdd452f4a5c77165037bc06d3e
PadalkoD <145016443+PadalkoD@users.noreply.github.com>
2023-10-26 14:21:48 +0300

    Add files via upload

---
+++ ss.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    srand(time(NULL));
+    setlocale(LC_ALL, "");
+    int plWin = 0;
+    int pcWin = 0;
+    const int coutWins = 3;
+    const int coutAttempts = 5;
+    const int minRange = 1;
+    const int maxRange = 100;
+    int pcNum, plNum;
+
+    while (true) {
+        system("cls");
+        cout << "Компьютер загадал число, попробуй отгадать";
+        pcNum = minRange + rand() % (maxRange + 1 - minRange);
+        for (int i = 0; i < coutAttempts; i++) {
+
+        
+            bool flag = false; // флаг для определения угадано число или нет
+            cout << "введите число:  " << endl;
+            cin >> plNum;
+            if (plNum > pcNum)
+            cout << "загаданное число меньше" << endl;
+            else if (plNum < pcNum)
+                cout << "загаданное число больше" << endl;
+            else {
+                cout << "Вы угадали число " << endl;
+            flag = true;
+            break;
+
+            (flag) ? plWin++ : pcWin++;
+            if (plWin >= coutWins) {
+                cout << "победа игрока со счётом - " << plWin << "";
+         
+        }
+    }
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
