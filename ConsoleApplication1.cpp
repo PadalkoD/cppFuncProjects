3ddb81e46202f8f2d925311cf714e0eabb2d6edc
PadalkoD <145016443+PadalkoD@users.noreply.github.com>
2024-01-18 16:02:30 +0430

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,75 @@
+﻿
+#include <iostream>
+using namespace std;
+//void printRectangle(int N, int K) {
+//    for (int i = 0; i < N; i++) {
+//        for (int j = 0; j < K; j++) {
+//            cout << "*";
+//        }
+//     cout << endl;
+//    }
+//}
+
+//void simpleKrealin(int a) {
+//	for (int i = 1; i <= 9; i++) {
+//		if (a % i == 0);
+//	}
+//}
+
+//void cubibNumber(int a) {
+//	cout << pow(a, 3);
+//}
+//
+//void maxNumber(int a, int b) {
+//	if (a > b) cout << a;
+//	else cout << b;
+//}
+//
+//bool chislo(int a) {
+//	if (a > 0) return true;
+//	else return false;
+//}
+
+
+//void NumberFaktorial(int a, int sum) {
+//	for (int i = 1; i <= a; i++) {
+//		sum  *= i;
+//	}
+//
+//}
+
+//bool simpleNum(int n) {
+//	for (int i = 2; i <= sqrt(n); i++) {
+//		if (n % i == 0) return false;
+//
+//	}
+//	return true;
+//}
+
+
+
+//bool plus(float a) {
+//	if (a > 0) return true;
+//	return false;
+//}
+
+
+
+
+
+
+
+int main()
+{
+
+
+
+
+
+
+
+
+
+
+
+}
